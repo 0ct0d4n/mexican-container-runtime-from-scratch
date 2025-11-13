8412031048914b45b1fde288d726b8bbf4817dd7
danielvera <danyelmorales1991@gmail.com>
2025-11-12 22:02:59 -0800

    to add rootfs catalog

--- pkg/util/rootfs/rootfs.go
+++ pkg/util/rootfs/rootfs.go
@@ -12,53 +12,66 @@ type RootFSEntry struct {
 	URL  string
 }
 
-var rootfsCatalog = map[string][]RootFSEntry{
-	"alpine": {
+type DistroType string
+
+const ALPINE DistroType = "ALPINE"
+const UBUNTU DistroType = "UBUNTU"
+const DEBIAN DistroType = "DEBIAN"
+const BUSYBOX DistroType = "BUSYBOX"
+const ARCH DistroType = "ARCH"
+const CENTOS DistroType = "CENTOS"
+const FEDORA DistroType = "FEDORA"
+const KALI DistroType = "KALI"
+const VOID DistroType = "VOID"
+const OPENSUSE DistroType = "OPENSUSE"
+
+var rootfsCatalog = map[DistroType][]RootFSEntry{
+	ALPINE: {
 		{Arch: "arm64", URL: "https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/aarch64/alpine-minirootfs-3.20.0-aarch64.tar.gz"},
 		{Arch: "amd64", URL: "https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/x86_64/alpine-minirootfs-3.20.0-x86_64.tar.gz"},
 	},
 
-	"ubuntu": {
+	UBUNTU: {
 		{Arch: "arm64", URL: "https://partner-images.canonical.com/core/jammy/current/ubuntu-jammy-core-cloudimg-arm64-root.tar.gz"},
 		{Arch: "amd64", URL: "https://partner-images.canonical.com/core/jammy/current/ubuntu-jammy-core-cloudimg-amd64-root.tar.gz"},
 	},
 
-	"debian": {
+	DEBIAN: {
 		{Arch: "arm64", URL: "https://cloud.debian.org/images/cloud/bullseye/latest/debian-11-genericcloud-arm64-root.tar.xz"},
 		{Arch: "amd64", URL: "https://cloud.debian.org/images/cloud/bullseye/latest/debian-11-genericcloud-amd64-root.tar.xz"},
 	},
 
-	"busybox": {
+	BUSYBOX: {
 		{Arch: "arm64", URL: "https://landley.net/aboriginal/downloads/busybox-rootfs-arm64.tar.gz"},
 		{Arch: "amd64", URL: "https://landley.net/aboriginal/downloads/busybox-rootfs-x86_64.tar.gz"},
 	},
 
-	"arch": {
+	ARCH: {
 		{Arch: "arm64", URL: "https://geo.mirror.pkgbuild.com/iso/latest/archlinux-bootstrap-latest-aarch64.tar.gz"},
 		{Arch: "amd64", URL: "https://geo.mirror.pkgbuild.com/iso/latest/archlinux-bootstrap-latest-x86_64.tar.gz"},
 	},
 
-	"centos": {
+	CENTOS: {
 		{Arch: "arm64", URL: "https://cloud.centos.org/centos/9-stream/aarch64/images/CentOS-Stream-GenericCloud-9-20240212.0.aarch64.qcow2"},
 		{Arch: "amd64", URL: "https://cloud.centos.org/centos/9-stream/x86_64/images/CentOS-Stream-GenericCloud-9-20240212.0.x86_64.qcow2"},
 	},
 
-	"fedora": {
+	FEDORA: {
 		{Arch: "arm64", URL: "https://download.fedoraproject.org/pub/fedora/linux/releases/40/Cloud/aarch64/images/Fedora-Cloud-Base-40-1.15.aarch64.raw.xz"},
 		{Arch: "amd64", URL: "https://download.fedoraproject.org/pub/fedora/linux/releases/40/Cloud/x86_64/images/Fedora-Cloud-Base-40-1.15.x86_64.raw.xz"},
 	},
 
-	"kali": {
+	KALI: {
 		{Arch: "arm64", URL: "https://cdimage.kali.org/kali-2024.2/kali-linux-2024.2-arm64-rootfs.tar.xz"},
 		{Arch: "amd64", URL: "https://cdimage.kali.org/kali-2024.2/kali-linux-2024.2-amd64-rootfs.tar.xz"},
 	},
 
-	"void": {
+	VOID: {
 		{Arch: "arm64", URL: "https://repo-default.voidlinux.org/live/current/void-aarch64-ROOTFS-20240315.tar.xz"},
 		{Arch: "amd64", URL: "https://repo-default.voidlinux.org/live/current/void-x86_64-ROOTFS-20240315.tar.xz"},
 	},
 
-	"opensuse": {
+	OPENSUSE: {
 		{Arch: "arm64", URL: "https://download.opensuse.org/ports/aarch64/tumbleweed/appliances/openSUSE-Tumbleweed-aarch64-RootFS.tar.xz"},
 		{Arch: "amd64", URL: "https://download.opensuse.org/tumbleweed/appliances/openSUSE-Tumbleweed-x86_64-RootFS.tar.xz"},
 	},
@@ -81,13 +94,14 @@ func normalizeArch(a string) string {
 // If arch = "" → auto-detect using runtime.GOARCH.
 func GetRootFSURL(arch, distro string) (string, error) {
 	distro = strings.ToLower(distro)
+	distroKey := DistroType(strings.ToUpper(distro))
 	if arch == "" {
 		arch = runtime.GOARCH
 	}
 
 	arch = normalizeArch(arch)
 
-	entries, ok := rootfsCatalog[distro]
+	entries, ok := rootfsCatalog[distroKey]
 	if !ok {
 		return "", fmt.Errorf("unsupported distro: %s", distro)
 	}
