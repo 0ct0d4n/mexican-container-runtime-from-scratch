5c1e10f1b35cd535ea7743b3897cde1b402d31ab
danielvera <danyelmorales1991@gmail.com>
2025-11-12 20:35:22 -0800

    desbloquear el consumer principal del demonio

--- pkg/server/commands.go
+++ pkg/server/commands.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"golang.org/x/crypto/ssh"
-	"log"
 )
 
 func decodePayloadFromChannel(ch ssh.Channel) (*model.RunRequest, error) {
@@ -17,13 +16,4 @@ func decodePayloadFromChannel(ch ssh.Channel) (*model.RunRequest, error) {
 	}
 
 	return &runRequest, nil
-}
-
-// sendMessage writes a message to the SSH channel.
-func sendMessage(ch ssh.Channel, msg string) error {
-	if _, err := ch.Write([]byte(msg)); err != nil {
-		log.Printf("Error: failed to write to SSH channel: %v", err)
-		return err
-	}
-	return nil
 }
