cd3ff84dd9cc8bcabc1ddc6bb5e70dba647ab2f7
danielvera <danyelmorales1991@gmail.com>
2025-11-14 20:46:35 -0800

    adding init-container mode

--- pkg/server/rootfs/manager.go
+++ pkg/server/rootfs/manager.go
@@ -135,5 +135,5 @@ func (c *RootFSInstallationConfig) enterChroot() error {
 }
 
 func (c *RootFSInstallationConfig) execShell() error {
-	return syscall.Exec("/bin/sh", []string{"/bin/sh", "-c", "echo CONTAINER_IS_WORKING_NOW!; ls /"}, os.Environ())
+	return syscall.Exec("/bin/sh", []string{"/bin/sh", "-c", "echo CONTAINER_IS_WORKING_NOW!; "}, os.Environ())
 }
